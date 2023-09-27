782e7cf0b2a81db199b1dd80a31bc798f3ea803e
simsonthatheus.thatheus <simsonthatheus.thatheus@bt.com>
2023-09-27 15:37:41 +0100

    version

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -107,7 +107,7 @@ export const config: Options.Testrunner = {
     baseUrl: 'http://localhost',
     //
     // Default timeout for all waitFor* commands.
-    waitforTimeout: 10000,
+    waitforTimeout: 100000,
     //
     // Default timeout in milliseconds for request
     // if browser driver or grid doesn't send response
@@ -120,7 +120,7 @@ export const config: Options.Testrunner = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: [['chromedriver',{drivers:{chrome:"114.0.5735.90"}}]],
+    services: [['chromedriver',{drivers:{chrome:"114.0.3"}}]],
     //
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
