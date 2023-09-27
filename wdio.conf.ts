3a4da4d8a07aa9f474b21b7356e319554b3d5740
simsonthatheus.thatheus <simsonthatheus.thatheus@bt.com>
2023-09-27 22:28:13 +0100

    latest

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -121,7 +121,7 @@ export const config: Options.Testrunner = {
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
     services: [
-        'chromedriver'],
+        ['chromedriver',{drivers:{chrome:"latest"}}]],
     //
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
